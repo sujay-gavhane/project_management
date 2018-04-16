a1b6d31735cd4bed78dc3caec98c96a4927954e4
sujay-gavhane <sujay.gavhane@gmail.com>
2018-04-17 00:52:15 +0530

    Added project-wise pie chart functionality

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -3,6 +3,7 @@ class Feature < ActiveRecord::Base
   belongs_to :project
   scope :by_status_project_developer, ->(project_id, developer_id, status) { where(status: status, project_id: project_id, developer_id: developer_id)}
   scope :status_by_project_manager, ->(project_id, status) { where(status: status, project_id: project_id)}
+  scope :by_status, -> (project_id, status){where(status: status, project_id: project_id)}
   
   validates :title, presence: true
   validates :developer_id, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     collection do
       patch :assign_project
     end
+    member do
+      get :pie_chart
+    end
     resources :features
   end
 
